7f66ac89e678a1c534f16e830524cd7125a73944
Luis Barrado <ljbarrado@gmail.com>
2021-05-30 16:36:59 +0200

    Rama correcta

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/mars', function (req, res) {
+  res.send('Hello Mars!\n');
+});
+
+app.get('/luis', function (req, res) {
+  res.send('Hello Luis!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
